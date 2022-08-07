421c5759d230e85dbe14624d6bcd3258d8ee5bdb
Sudarshan Uprety <suprety4444@gmail.com>
2022-08-07 00:19:37 -0400

    Shop page

---
+++ javascript/productlist.js
@@ -0,0 +1,74 @@
+var products = {
+    1: {
+      name: "Side Bag",
+      desc: "Easy to carry side bag.",
+      img: "bag.webp",
+      price: 17
+    },
+    2: {
+      name: "Belt",
+      desc: "Genuine leather belt.",
+      img: "belt.webp",
+      price: 12
+    },
+    3: {
+      name: "Bracelet",
+      desc: "High quality bracelet.",
+      img: "bracelet.webp",
+      price: 120
+    },
+    4: {
+      name: "Cap",
+      desc: "Summer Cap for sexy man.",
+      img: "cap.webp",
+      price: 20
+    },
+    5: {
+      name: "Chain",
+      desc: "Neck Chain original Diamond.",
+      img: "chain.webp",
+      price: 5000
+    },
+    6: {
+      name: "Glass",
+      desc: "Reading glass, Scratch proof",
+      img: "glass.webp",
+      price: 842
+    },
+    7: {
+      name: "Hat",
+      desc: "Stylish hat for gentleman",
+      img: "hat.webp",
+      price: 842
+    },
+    8: {
+      name: "Perfume",
+      desc: "Long lasting perfume man",
+      img: "perfume.webp",
+      price: 842
+    },
+    9: {
+      name: "Ring",
+      desc: "Original Ring for him",
+      img: "ring.webp",
+      price: 842
+    },
+    10: {
+      name: "Tie",
+      desc: "Gentlemen's tie",
+      img: "tie.webp",
+      price: 842
+    },
+    11: {
+      name: "T-shirt",
+      desc: "Black original cotton t-shirt",
+      img: "tshirt.jpg",
+      price: 842
+    },
+    12: {
+      name: "wallet",
+      desc: "Leather wallet",
+      img: "wallet.jpg",
+      price: 842
+    },
+  };
--- javascript/products.js
+++ javascript/products.js
@@ -1,78 +1,4 @@
 // json data for list of products
-var products = {
-  1: {
-    name: "Side Bag",
-    desc: "Easy to carry side bag.",
-    img: "bag.webp",
-    price: 17
-  },
-  2: {
-    name: "Belt",
-    desc: "Genuine leather belt.",
-    img: "belt.webp",
-    price: 12
-  },
-  3: {
-    name: "Bracelet",
-    desc: "High quality bracelet.",
-    img: "bracelet.webp",
-    price: 120
-  },
-  4: {
-    name: "Cap",
-    desc: "Summer Cap for sexy man.",
-    img: "cap.webp",
-    price: 20
-  },
-  5: {
-    name: "Chain",
-    desc: "Neck Chain original Diamond.",
-    img: "chain.webp",
-    price: 5000
-  },
-  6: {
-    name: "Glass",
-    desc: "Reading glass, Scratch proof",
-    img: "glass.webp",
-    price: 842
-  },
-  7: {
-    name: "Hat",
-    desc: "Stylish hat for gentleman",
-    img: "hat.webp",
-    price: 842
-  },
-  8: {
-    name: "Perfume",
-    desc: "Long lasting perfume man",
-    img: "perfume.webp",
-    price: 842
-  },
-  9: {
-    name: "Ring",
-    desc: "Original Ring for him",
-    img: "ring.webp",
-    price: 842
-  },
-  10: {
-    name: "Tie",
-    desc: "Gentlemen's tie",
-    img: "tie.webp",
-    price: 842
-  },
-  11: {
-    name: "T-shirt",
-    desc: "Black original cotton t-shirt",
-    img: "tshirt.jpg",
-    price: 842
-  },
-  12: {
-    name: "wallet",
-    desc: "Leather wallet",
-    img: "wallet.jpg",
-    price: 842
-  },
-};
 
 var cart = {
   // Cart properties
@@ -138,9 +64,7 @@ var cart = {
 
     // check cart is empty
     if (empty) {
-      item = document.createElement("div");
-      item.innerHTML = "<div id='empty-message'>Cart is empty</div>";
-      cart.hItems.appendChild(item);
+      cart.hItems.appendChild(document.getElementById("empty-template").content.cloneNode(true));
     }
 
     // if not emplty list cart items
@@ -211,6 +135,9 @@ var cart = {
     alert("Do later");
   }
 };
-window.addEventListener("DOMContentLoaded", cart.init);
+window.addEventListener("DOMContentLoaded", ()=>{
+  cart.init();
+  
+});
 
 
